e91cf93c3add308ccdb87c4d4160f548e601ffbd
Remi Paulmier <remi.paulmier@gmail.com>
2016-02-10 13:55:51 +0100

    no more globals

--- cloud-cabling-matrix.py
+++ cloud-cabling-matrix.py
@@ -3,16 +3,7 @@
 from pysdn.devices import PatchPanel, Switch, LineCard, Port
 from pysdn.utils import CablingMatrix
 
-m = CablingMatrix()
-
-po1 = PatchPanel(connector=Port.SC, place='BETA R-2 left', u=1, name='tiroir operateur1', size=12)
-po2 = PatchPanel(connector=Port.SC, place='BETA R-2 left', u=2, name='tiroir operateur2', size=12)
-p1 = PatchPanel(connector=Port.LC, place='BETA R-2 right', u=3, name='tiroir2', size=24)
-p2 = PatchPanel(connector=Port.LC, place='ALPHA R+3',u=1, name='tiroir1', size=6)
-p3 = PatchPanel(connector=Port.LC, place='ALPHA R+4a', u=1, name='tiroir1', size=6)
-p4 = PatchPanel(connector=Port.LC, place='ALPHA R+4b', u=1, name='tiroir1', size=6)
-
-def brasse_le_moins_2():
+def brasse_le_moins_2(matrix, p1, po1, po2):
     s1 = Switch(place='BETA R-2 right', u=10, name='sw-agg-1')
     s1.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
     s2 = Switch(place='BETA R-2 right', u=11, name='sw-agg-2')
@@ -42,12 +33,12 @@ def brasse_le_moins_2():
     r2.cards[0].ports[0].connect(po2.ports[0])
 
     # add devices to matrix
-    m.add_switch(s1)
-    m.add_switch(s2)
-    m.add_switch(r1)
-    m.add_switch(r2)
+    matrix.add_switch(s1)
+    matrix.add_switch(s2)
+    matrix.add_switch(r1)
+    matrix.add_switch(r2)
 
-def brasse_le_plus_3():
+def brasse_le_plus_3(matrix, uplinks):
     s1 = Switch(place='ALPHA R+3', u=9, name='1')
     s1.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
     s2 = Switch(place='ALPHA R+3', u=11, name='2')
@@ -57,20 +48,19 @@ def brasse_le_plus_3():
     s4 = Switch(place='ALPHA R+3', u=15, name='4')
     s4.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
 
-    s1.cards[0].ports[22].connect(p2.ports[0])
-    s1.cards[0].ports[23].connect(p2.ports[1])
-    s2.cards[0].ports[22].connect(p2.ports[2])
-    s2.cards[0].ports[23].connect(p2.ports[3])
-    s3.cards[0].ports[23].connect(p2.ports[4])
-    s4.cards[0].ports[23].connect(p2.ports[5])
+    s1.cards[0].ports[22].connect(uplinks[0])
+    s1.cards[0].ports[23].connect(uplinks[1])
+    s2.cards[0].ports[22].connect(uplinks[2])
+    s2.cards[0].ports[23].connect(uplinks[3])
+    s3.cards[0].ports[23].connect(uplinks[4])
+    s4.cards[0].ports[23].connect(uplinks[5])
 
-    m.add_switch(s1)
-    m.add_switch(s2)
-    m.add_switch(s3)
-    m.add_switch(s4)
+    matrix.add_switch(s1)
+    matrix.add_switch(s2)
+    matrix.add_switch(s3)
+    matrix.add_switch(s4)
 
-def brasse_le_plus_4():
-    # r+4a
+def brasse_le_plus_4a(matrix, uplinks):
     s1 = Switch(place='BETA R+4a', u=9, name='1')
     s1.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
     s2 = Switch(place='BETA R+4a', u=11, name='2')
@@ -78,40 +68,50 @@ def brasse_le_plus_4():
     s3 = Switch(place='BETA R+4a', u=13, name='3')
     s3.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
 
-    s1.cards[0].ports[22].connect(p3.ports[0])
-    s1.cards[0].ports[23].connect(p3.ports[1])
-    s2.cards[0].ports[22].connect(p3.ports[2])
-    s2.cards[0].ports[23].connect(p3.ports[3])
-    s3.cards[0].ports[22].connect(p3.ports[4])
-    s3.cards[0].ports[23].connect(p3.ports[5])
+    s1.cards[0].ports[22].connect(uplinks[0])
+    s1.cards[0].ports[23].connect(uplinks[1])
+    s2.cards[0].ports[22].connect(uplinks[2])
+    s2.cards[0].ports[23].connect(uplinks[3])
+    s3.cards[0].ports[22].connect(uplinks[4])
+    s3.cards[0].ports[23].connect(uplinks[5])
 
-    m.add_switch(s1)
-    m.add_switch(s2)
-    m.add_switch(s3)
+    matrix.add_switch(s1)
+    matrix.add_switch(s2)
+    matrix.add_switch(s3)
 
-    # r+4b
+def brasse_le_plus_4b(matrix, uplinks):
     s1 = Switch(place='BETA R+4b', u=7, name='1')
     s1.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
     s2 = Switch(place='BETA R+4b', u=9, name='2')
     s2.add_line_card(LineCard(connector=Port.LC, name='', portprefix='port', size=24))
 
-    s1.cards[0].ports[22].connect(p4.ports[0])
-    s1.cards[0].ports[23].connect(p4.ports[1])
-    s2.cards[0].ports[22].connect(p4.ports[2])
-    s2.cards[0].ports[23].connect(p4.ports[3])
+    s1.cards[0].ports[22].connect(uplinks[0])
+    s1.cards[0].ports[23].connect(uplinks[1])
+    s2.cards[0].ports[22].connect(uplinks[2])
+    s2.cards[0].ports[23].connect(uplinks[3])
 
-    m.add_switch(s1)
-    m.add_switch(s2)
+    matrix.add_switch(s1)
+    matrix.add_switch(s2)
 
 def main():
+    m = CablingMatrix()
+
+    po1 = PatchPanel(connector=Port.SC, place='BETA R-2 left', u=1, name='tiroir operateur1', size=12)
+    po2 = PatchPanel(connector=Port.SC, place='BETA R-2 left', u=2, name='tiroir operateur2', size=12)
+    p1 = PatchPanel(connector=Port.LC, place='BETA R-2 right', u=3, name='tiroir2', size=24)
+    p2 = PatchPanel(connector=Port.LC, place='ALPHA R+3',u=1, name='tiroir1', size=6)
+    p3 = PatchPanel(connector=Port.LC, place='ALPHA R+4a', u=1, name='tiroir1', size=6)
+    p4 = PatchPanel(connector=Port.LC, place='ALPHA R+4b', u=1, name='tiroir1', size=6)
+
     # cross-co
     p1.multi_x_connect(position=1, peer=p2, peerPosition=1, size=6)
     p1.multi_x_connect(position=7, peer=p3, peerPosition=1, size=6)
     p1.multi_x_connect(position=13, peer=p4, peerPosition=1, size=6)
 
-    brasse_le_moins_2()
-    brasse_le_plus_3()
-    brasse_le_plus_4()
+    brasse_le_moins_2(matrix=m, p1=p1, po1=po1, po2=po2)
+    brasse_le_plus_3(matrix=m, uplinks=p2.ports)
+    brasse_le_plus_4a(matrix=m, uplinks=p3.ports)
+    brasse_le_plus_4b(matrix=m, uplinks=p4.ports)
 
     m.dump()
 
