723e3e9cf581a28c49ce44f53a0afa96119acb8d
Remi Paulmier <remi.paulmier@gmail.com>
2016-02-10 15:57:30 +0100

    adds wifi and desk stuff to R+4a

--- cloud-cabling-matrix.py
+++ cloud-cabling-matrix.py
@@ -104,9 +104,37 @@ def brasse_le_plus_4a(matrix, uplinks):
     s3.cards[0].ports[22].connect(uplinks[4])
     s3.cards[0].ports[23].connect(uplinks[5])
 
+    # patch desktop switches
+    p1 = PatchPanel(connector=Port.LC, place='BETA R+4a', u=3, name='', size=24)
+    p2 = PatchPanel(connector=Port.LC, place='BETA R+4a', u=5, name='', size=24)
+    p3 = PatchPanel(connector=Port.LC, place='BETA R+4a', u=7, name='', size=24)
+
+    desks = 43
+    agg_ports = available_ports(need=desks, devices=(s1, s2, s3, ))
+    patch_ports = available_ports(need=desks, devices=(p1, p2, p3, ))
+
+    for i in range(0, desks):
+        agg_ports[i].connect(patch_ports[i])
+
+    # patch wifi switch
+    sw1 = Switch(place='BETA R+4a', u=15, name='wifi')
+    sw1.add_line_card(LineCard(connector=Port.RJ45, name='', portprefix='port', size=24))
+    agg_ports = available_ports(need=1, devices=(s1, s2, s3, ))
+    sw1.cards[0].ports[23].connector = Port.LC
+    sw1.cards[0].ports[23].connect(agg_ports[-1])
+
+    pc1 = PatchPanel(connector=Port.RJ45, place='ALPHA R+3', u=20, name='tiroir cuivre', size=24)
+    wifis = 23
+    agg_ports = available_ports(need=wifis, devices=(sw1, ))
+    patch_ports = available_ports(need=wifis, devices=(pc1, ))
+
+    for i in range(0, wifis):
+        agg_ports[i].connect(patch_ports[i])
+
     matrix.add_switch(s1)
     matrix.add_switch(s2)
     matrix.add_switch(s3)
+    matrix.add_switch(sw1)
 
 def brasse_le_plus_4b(matrix, uplinks):
     s1 = Switch(place='BETA R+4b', u=7, name='1')
