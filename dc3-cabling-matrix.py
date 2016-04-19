cf247c48f4179390d68d304af69cf338c946900f
Remi Paulmier <remi.paulmier@gmail.com>
2016-04-19 16:09:49 +0200

    real device positions

--- dc3-cabling-matrix.py
+++ dc3-cabling-matrix.py
@@ -11,47 +11,51 @@ def brasse_la_baie_telecom():
     # creation des racks
     r = Rack(name='DC3/4.2/F23')
 
-    ## FO ##
-    # 96 FO => 48 duplex
-    po = PatchPanel(connector=Connector.LC, name='po', size=48)
-    r.rack(po, u=1, height=1)
-
-    pop = PatchPanel(connector=Connector.LC, name='pop', size=24)
+    # U3: operateurs: 12 duplex SC
+    pop = PatchPanel(connector=Connector.SC, name='pop', size=24)
     r.rack(pop, u=3, height=1)
 
-    # 5-6: ASR1
+    # U5: 96 FO
+    po = PatchPanel(connector=Connector.LC, name='po', size=96)
+    r.rack(po, u=5, height=1)
+
+    # U7-8: 28 cuivre
+    pc = PatchPanel(connector=Connector.RJ45, name='pc', size=28)
+    r.rack(pc, u=7, height=2)
+
+    # U10: mux
+
+    # U12-13: ASR1
     asr1 = ASR9001(name='core-01')
     asr1.add_module(LineCard(connector=Connector.LC, name='R/SFP', base=0, size=20))
     asr1.add_module(LineCard(connector=Connector.LC, name='L/XFP', base=0, size=4))
-    r.rack(asr1, u=5, height=2)
-    # 8-9: ASR2
+    r.rack(asr1, u=12, height=2)
+    # U15-16: ASR2
     asr2 = ASR9001(name='core-02')
     asr2.add_module(LineCard(connector=Connector.LC, name='R/SFP', base=0, size=20))
     asr2.add_module(LineCard(connector=Connector.LC, name='L/XFP', base=0, size=4))
-    r.rack(asr2, u=8, height=2)
+    r.rack(asr2, u=15, height=2)
 
-    # 11: mux
-
-    # 13: dist1
-    nex1 = Nexus3064_X(name='dist-01')
-    r.rack(nex1, u=13, height=1)
-    # 15: dist2
-    nex2 = Nexus3064_X(name='dist-02')
-    r.rack(nex2, u=15, height=1)
-
-    # 17-18: arbor
-
-    # 20: oob
+    # U18: oob
     sf = SF300_24(name='sw-oob')
-    r.rack(sf, u=20, height=1)
+    r.rack(sf, u=18, height=1)
 
-    # 22: console
+    # U20-21: PDU1
+
+    # U23-24: PDU2
+
+    # U26: dist1
+    nex1 = Nexus3064_X(name='dist-01')
+    r.rack(nex1, u=26, height=1)
+    # U28: dist2
+    nex2 = Nexus3064_X(name='dist-02')
+    r.rack(nex2, u=28, height=1)
+
+    # U30: console
     og = CM4132(name='console')
-    r.rack(og, u=22, height=1)
+    r.rack(og, u=30, height=1)
 
-    # 24-25: cuivre
-    pc = PatchPanel(connector=Connector.RJ45, name='pc', size=48)
-    r.rack(pc, u=24, height=2)
+    # U32-33: arbor
 
     # brassage console
     for (port, device) in ( ('port1', asr1), ('port2', asr2), ('port3', nex1), ('port4', nex2) ):
